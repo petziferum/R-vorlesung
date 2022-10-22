fb7fa389cf619fa6f294a97819153df66a453172
Petzi <daboarderpjb@gmail.com>
2022-10-22 15:12:51 +0200

    remove NA

--- vorlesung01.R
+++ vorlesung01.R
@@ -42,3 +42,47 @@ df_2 <- df_2[ , c("avg_aktien", "aktien", "aktien_2", "quartal", "lockdown")]
 df_2
 
 data()
+
+head(mtcars) #head zeigt immer nur 6 Zeilen
+df <- mtcars
+head(df)
+View(df)
+
+
+#Übung 3 Datei --> F:/R/vorlesung/01_R...html
+
+#1 ------------------------
+Alter <- c(25,22,26)
+Gewicht <- c(85,76,62)
+Geschlecht <- c("M","M","F")
+Namen <- c("Sam", "Frank", "Anna")
+
+pf <- data.frame(row.names = Namen, Alter, Gewicht, Geschlecht)
+pf
+
+pf["Anna", "Alter"] <- NA
+mean(pf$Alter)
+mean(pf$Alter, na.rm=TRUE)
+
+is.data.frame(pf)
+is.na(pf$Alter)
+
+#2 -------------------
+print(is.data.frame(mtcars))
+
+#5------------------------------------
+cars <- as.data.frame(mtcars)
+cars
+head(cars) 
+
+#6 ----------------------------------
+mean(cars$mpg)
+
+#7 ---------------------------------
+cylinder <- subset(cars, cyl == 6)
+cylinder
+
+#8 ---------------------------------
+
+
+#9 ---------------------------------
